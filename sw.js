539aa619d931cdad7881343aff35f35320f2151e
Nguyen Thai Hoang <hoangthai.personal@gmail.com>
2026-09-09 15:15:03 +0700

    fix: refresh splash cache without ring effects

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Rootflow service worker. */
-var CACHE='rootflow-ui-2026-09-09-brand-r3';
-var ASSETS=['./','./index.html','./styles.css','./capital.css','./brand/rootflow-theme.css','./brand/rootflow-splash.css?v=20260909-brand-r3','./brand/rootflow-mark.png?v=20260909-brand-r3','./brand/rootflow-icon.png?v=20260909-brand-r3','./operating-policy.js','./domain.js','./cashflow-domain.js','./capital-domain.js','./store.js','./selftest.js','./capital-ui.js','./app.js','./manifest.json?v=20260909-brand-r3','./vendor/react.production.min.js','./vendor/react-dom.production.min.js'];
+var CACHE='rootflow-ui-2026-09-09-brand-r4-norings';
+var ASSETS=['./','./index.html','./styles.css','./capital.css','./brand/rootflow-theme.css','./brand/rootflow-splash.css?v=20260909-brand-r4-norings','./brand/rootflow-mark.png?v=20260909-brand-r3','./brand/rootflow-icon.png?v=20260909-brand-r3','./operating-policy.js','./domain.js','./cashflow-domain.js','./capital-domain.js','./store.js','./selftest.js','./capital-ui.js','./app.js','./manifest.json?v=20260909-brand-r3','./vendor/react.production.min.js','./vendor/react-dom.production.min.js'];
 self.addEventListener('install',function(event){event.waitUntil(caches.open(CACHE).then(function(cache){return cache.addAll(ASSETS)}).then(function(){return self.skipWaiting()}))});
 self.addEventListener('activate',function(event){event.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(key){return key!==CACHE}).map(function(key){return caches.delete(key)}))}).then(function(){return self.clients.claim()}))});
 function ok(res){return res&&res.ok&&res.type==='basic'}
